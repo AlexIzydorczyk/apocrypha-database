4ba6a406b43eaca534de639ba212e15d4eb7db02
ben-macpherson <benmacpherson2000@gmail.com>
2021-12-30 10:45:26 -0600

    initial generation & devise setup

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable,  and :omniauthable, :registerable, :recoverable, :rememberable
+  devise :database_authenticatable, :validatable, :trackable
+end
