c80afb8535b7ff2ee13cea7d80c8e560c97f3426
ben-macpherson <benmacpherson2000@gmail.com>
2022-01-17 14:44:54 -0600

    added autosave

--- app/assets/javascripts/additional.js
+++ app/assets/javascripts/additional.js
@@ -1,6 +1,22 @@
-console.log('file loaded');
+// --------- declarations ---------
+
+var changeMade = false;
+
+var autoSaveFun = function autosave() {
+  if(changeMade){
+    $('form').each(function () {
+      $(this).ajaxSubmit(function() {
+        changeMade = false;
+        window.SnackBar({message: "Autosaved form"})
+      })
+    });
+  }
+}
+
+// --------- rdy ---------
 
 $(document).ready(function() {
+  
   $("textarea").each(function () {
     this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
   }).on("input", function () {
@@ -8,4 +24,16 @@ $(document).ready(function() {
     this.style.height = "auto";
     this.style.height = (this.scrollHeight) + "px";
   });
+
+  $('form').ajaxForm({success: function() {
+    window.SnackBar({message: "Saved form"})
+  }});
+
+  setInterval(autoSaveFun,10000);
+
+  $('input, select').change(function() {
+    changeMade = true;
+  });
+
+
 })
--- app/models/manuscript.rb
+++ app/models/manuscript.rb
@@ -1,26 +1,10 @@
 class Manuscript < ApplicationRecord
   belongs_to :institution, optional: true
   has_many :language_references, as: :record
-  # has_many :languages, through: :languages_references, as: :record
+  has_many :languages, through: :language_references, as: :record
   has_many :booklets
   has_many :modern_source_references, as: :record
   has_many :modern_sources, through: :modern_source_references
   has_many :person_references
   has_many :correspondents, class_name: "Person", through: :person_references
-
-  attr_writer :languages
-
-  def languages=(value)
-    puts 'inside attr writer'
-    value.select{ |val| val.present? }.each{ |val|
-      puts 'value'.blue
-      puts val
-      lr = LanguageReference.find_or_initilize_by(
-        record: self,
-        language_id: val,
-      )
-      lr.save!
-    }
-  end
-
 end
