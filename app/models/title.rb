8573713c1bdaee4519ebe46ea7676637cd6d27e2
joaoacflores <joaoacflores@gmail.com>
2022-01-19 09:56:20 -0600

    partial content for manuscript

--- app/models/title.rb
+++ app/models/title.rb
@@ -2,4 +2,9 @@ class Title < ApplicationRecord
   belongs_to :apocryphon, optional: true
   belongs_to :language, optional: true
   has_many :contents
+
+  def title_english
+    #check for language
+    self.title_translation.present? ? self.title_translation : self.title_orig
+  end
 end
