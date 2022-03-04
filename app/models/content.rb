266294dd1aa05b4e3f2ea9138a191faf8929a920
ben-macpherson <benmacpherson2000@gmail.com>
2022-03-04 14:24:59 -0600

    tweaks from figma

--- app/models/content.rb
+++ app/models/content.rb
@@ -11,9 +11,9 @@ class Content < ApplicationRecord
     # needs to be updated to include incipit
     self.title.present? && self.title.title_translation.present? ? self.title.title_translation : (self.author.present? ? (self.author.first_name_vernacular + self.author.middle_name_vernacular + self.author.last_name_vernacular) : "")
     s = []
-    s.push("Title: " + self.title.title_english + " ") if self.title.present?
-    s.push("Author: " + self.author.full_name + " ") if self.author.present?
-    s.join(' | ')
+    s.push(self.title.title_english) if self.title.present?
+    s.push(self.author.full_name) if self.author.present?
+    s.join(', ')
   end
 
   def short_name
