c3367d0944ed8db75e03b514822c33b93663cf3e
ben-macpherson <benmacpherson2000@gmail.com>
2022-04-11 12:19:07 -0500

    Update modern_source.rb

--- app/models/modern_source.rb
+++ app/models/modern_source.rb
@@ -35,8 +35,6 @@ class ModernSource < ApplicationRecord
   end
 
   def set_display_name
-    puts 'running set display name for'.blue
-    puts self.inspect
     s = ""
     
     # name (initial)
@@ -147,8 +145,6 @@ class ModernSource < ApplicationRecord
     s = ""
     not_latin = self.writing_system.present? && self.writing_system != WritingSystem.find_by(name: "Latin")
     title = not_latin ? translit : orig
-    puts 'title'.red
-    puts title
     if title.present?
       s += '"' if show_quotes
       s += "<i>" if italics
@@ -159,8 +155,6 @@ class ModernSource < ApplicationRecord
     end
     # english = Language.find_or_create_by(language_name: 'English', requires_transliteration: false)
     s += "[" + transla + "]" unless !not_latin || transla.blank?
-    puts 'setting name as'.red
-    puts s
     s
   end
 
