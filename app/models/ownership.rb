1cca306e0889bf316f2a3f943ba430ba937bac70
joaoacflores <joaoacflores@gmail.com>
2022-05-13 15:22:43 -0500

    speed on master grid

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -16,21 +16,14 @@ class Ownership < ApplicationRecord
   end
 
   def display_name
-    [
-      ([
-        self.person.try(:full_name),
-        self.person.try(:years)
-      ].select{ |t| t.present? }.join(' ')),
-      self.institution.try(:display_name),
-      self.religious_order.try(:order_name)
-    ].select{ |s| s.present? }.join(', ')
+    [([self.person.try(:full_name), self.person.try(:years)].select{ |t| t.present? }.join(' ')), self.institution.try(:display_name), self.religious_order.try(:order_name)].select{ |s| s.present? }.join(', ')
   end
 
   def full_display_name
     text = self.display_name
-    text += "; " if self.display_name.present? && self.location.try(:city_region_country).present?
-    text += self.location.try(:city_region_country) if self.location.try(:city_region_country).present?
-    text += ". " if self.display_name.present? || self.location.try(:city_region_country).present?
+    text += "; " if self.display_name.present? && self.location.present?
+    text += self.location.try(:city_region_country) if self.location.present?
+    text += ". " if self.display_name.present? || self.location.present?
     text += self.provenance_notes
     text
   end
